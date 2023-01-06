a13bbd855d780b9c594a1f61e7685c99bf56824a
Aleksandr Revozyan <a.revozyan@gmail.com>
2023-01-06 18:17:25 +0500

    env aws

--- dev_stage/main.tf
+++ dev_stage/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = "1.3.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 
   backend "s3" {
     bucket         = "maintfstatebucketarevozyan"
