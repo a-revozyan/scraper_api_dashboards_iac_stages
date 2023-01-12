4baeef531ec52306cb9d109923a613b343cac27a
Aleksandr Revozyan <a.revozyan@gmail.com>
2023-01-12 15:02:35 +0500

    terraform apply - descriptions

--- dev_stage/second_step/main.tf
+++ dev_stage/second_step/main.tf
@@ -44,27 +44,3 @@ module "nlb" {
   frontend_subnet_ids   = data.terraform_remote_state.first_step.outputs.frontend-subnet_ids
   env                   = var.stage
 }
-#  ecs_app_task_role       = module.iam.app_task_role_arn
-#  ecs_app_execution_role  = module.iam.app_execution_role_arn
-#  sel_repository_arn      = module.ecr.aws_ecr_repository_arn_selenium
-#  sel_repository_name     = module.ecr.aws_ecr_repository_name_selenium
-#  api_repository_arn      = module.ecr.aws_ecr_repository_arn_api
-#  api_repository_name     = module.ecr.aws_ecr_repository_name_api
-#  dash_repository_arn     = module.ecr.aws_ecr_repository_arn_dash
-#  dash_repository_name    = module.ecr.aws_ecr_repository_name_dash
-#  backend_subnet          = module.vpc.backend-subnet_ids
-#  frontend_subnets        = module.vpc.frontend-subnet_ids
-#  api01_security_group    = module.sgs.api_sg_5050
-#  dash01_security_group   = module.sgs.dash_sg_5000
-#  target_group            = module.nlb.target_group
-#  target_group2           = module.nlb.target_group2
-#  env                     = var.stage
-#}
-#
-#module "nlb" {
-#  depends_on            = [module.vpc]
-#  source                = "../modules/nlb/"
-#  vpc_id                = module.vpc.vpc_id
-#  frontend_subnet_ids   = module.vpc.frontend-subnet_ids
-#  env                   = var.stage
-#}
